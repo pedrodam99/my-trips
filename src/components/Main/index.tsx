7c922c959939df6adae8fd9bdb49c5038b9537c8
Pedro <pedrodam99@gmail.com>
2022-10-25 10:49:42 -0300

    Configurando leaflet e Criando componente de Mapa

--- src/components/Main/index.tsx
+++
@@ -1,3 +0,0 @@
-const Main = () => <h1>Next JS na prática</h1>
-
-export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,12 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+          integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+          crossOrigin=""
+        />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
